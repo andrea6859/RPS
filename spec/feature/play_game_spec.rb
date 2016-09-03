f2b483a7fbd2df2795ebbaf00ea4e57ef692a142
Andrea Wildsmith <awildsmi@hotmail.com>
2016-09-03 14:21:11 +0100

    computer chooses weapon

--- spec/feature/play_game_spec.rb
+++ spec/feature/play_game_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
-feature 'marketer will be presented 3 choices' do
+feature 'play game' do
   scenario 'choose rock, paper, scissors' do
     sign_in_and_submit_name
     click_button 'Rock'
-    expect(page).to have_content 'You choose Rock'
-
-
+    expect(page).to have_content 'You chose Rock'
   end
+
+  scenario 'computer chooses "Rock"' do
+    sign_in_and_submit_name
+    click_button 'Rock'
+    expect(page).to have_content 'Computer chooses rock'
+  end
+
+
 end
