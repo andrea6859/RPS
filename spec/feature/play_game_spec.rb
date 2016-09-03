471c8721441c7832a4f20a6832ce5d358253cbf6
Andrea Wildsmith <awildsmi@hotmail.com>
2016-09-03 14:03:16 +0100

    user can select weapon

--- spec/feature/play_game_spec.rb
+++ spec/feature/play_game_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 feature 'marketer will be presented 3 choices' do
-  scenario 'choice of rock, paper, scissors' do
+  scenario 'choose rock, paper, scissors' do
     sign_in_and_submit_name
-    expect(page).to have_content 'Rock Paper Scissors'
+    click_button 'Rock'
+    expect(page).to have_content 'You choose Rock'
+
+
   end
 end
